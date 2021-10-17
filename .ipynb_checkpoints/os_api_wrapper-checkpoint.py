242ba70dde124d5383cb433ce64d0452a84b7d02
JDY <jonyamaoka@MacBook.local>
2021-10-17 12:17:45 -0400

    add topshot examples and data

--- .ipynb_checkpoints/os_api_wrapper-checkpoint.py
+++ .ipynb_checkpoints/os_api_wrapper-checkpoint.py
@@ -27,3 +27,28 @@ class openseacollection:
         
     def as_df(self): # cast attributes into df
         return pd.DataFrame({'Token ID':self.token_id,'Name':self.name})
+    
+
+class OpenSeaItem:
+    def __init__ (self, single_url):
+        self.single_url = single_url   #assign query url to "self.url"
+        self.get_single_data()  #run get_data method
+        self.get_single_attributes() #run get attribute method
+
+    def get_single_data(self):
+        self.single_data = requests.request("GET", self.single_url).json() #assign query json to "self.data"
+
+    def get_single_attributes(self):
+        self.asset_image = self.single_data['image_thumbnail_url']
+        self.currency_data = [x['symbol'] for x in self.single_data['collection']['payment_tokens']]
+        self.floor_price = self.single_data['collection']['stats']['floor_price']
+        self.total_supply = self.single_data['collection']['stats']['total_supply']
+        self.traits = [x['trait_type'] for x in self.single_data['traits']]
+        self.collection_name = self.single_data['collection']['name']
+    
+    def price_plot(self):
+        price_plot_chart = self.data['hist_price'].hvplot.line()
+        return price_plot_chart
+    
+    def as_df(self):
+        return pd.DataFrame({'Token ID':self.token_id,'Name':self.name})
